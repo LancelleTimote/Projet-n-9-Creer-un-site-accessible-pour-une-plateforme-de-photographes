3001c6c4771a4b4c9345f1050c9b5e6b7885e5a3
Timoté LANCELLE <timote.lancelle@outlook.fr>
2022-07-07 18:17:58 +0200

    Création du système de filtre, anticipation de l'ajout de fonction de like

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -38,10 +38,87 @@ function displayMedia(medias) {
     })
 }
 
+let isOpen = false;
+function filterDisplay() {
+    const icon = document.querySelector(".icon_filter");
+    const filterOptionBox = document.querySelector(".filter_select_options");
+    const selected = document.querySelector(".selected");
+
+    selected.onclick = () => {
+        if(isOpen) {
+            closeBox();
+        }else{
+            filterOptionBox.getElementsByClassName.display = "flex";
+            filterOptionBox.setAttribute("aria-expanded", "true");
+            icon.classList.add("icon_rotate");
+            filterOptionBox.classList.add("open")
+            isOpen = true;
+        }
+    }
+}
+
+function closeBox() {
+    const icon = document.querySelector(".icon_filter");
+    icon.classList.remove("icon_rotate");
+    const filterOptionBox = document.querySelector(".filter_select_options");
+
+    filterOptionBox.style.display = "none";
+    filterOptionBox.setAttribute("aria-expanded", "false");
+    return isOpen = false;
+}
+
+function sortMedias(medias) {
+    const filterOptions = document.querySelectorAll(".filter_option");
+    const selected = document.querySelector(".selected");
+
+    medias = medias?.sort((media1, media2) => {
+        return media2.likes - media1.likes;
+    })
+    displayMedia(medias);
+    displayLightbox(mediasList);
+
+    filterOptions?.forEach(filter => {
+        filter.onclick = (e) => {
+            switch(e.target.textContent) {
+                case "Popularité" :
+                    medias = medias?.sort((media1, media2) => {
+                        return media2.likes - media1.likes;
+                    });
+                break
+
+                case "Date" :
+                    medias = medias?.sort((media1, media2) => {
+                        return new Date(media2.date) - new Date(media1.date)
+                    });
+                break;
+
+                case "Titre" :
+                    medias = medias?.sort((media1, media2) => {
+                        return media1.title.localeCompare(media2.title);
+                    });
+                break
+            }
+
+            const buttonSelected = filter.textContent;
+            filter.textContent = selected.textContent;
+            selected.textContent = buttonSelected;
+
+            closeBox();
+            displayMedia(medias);
+            incrementLikes();
+            displayLightbox(mediasList);
+        }
+    })
+}
+
 async function init() {
     const photographer = await getPhotographer();
     const photographerData = photographerFactory(photographer);
     photographerData.getPhotographerProfile();
+
+    const medias = await getMedias();
+    filterDisplay();
+    sortMedias(medias);
 }
 
 init();
